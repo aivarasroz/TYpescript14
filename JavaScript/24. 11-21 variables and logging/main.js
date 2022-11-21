b1adc4f50f4bc75e78b62f3f252ef782f6228d7a
AivarasRoz <aivaras.roz@gmail.com>
2022-11-21 17:49:37 +0200

    24. Paskaita - JS intro (Variables) v1

---
+++ JavaScript/24. 11-21 variables and logging/main.js
@@ -0,0 +1,21 @@
+let age1 = 7;
+let age2= 15;
+let age3 = 33;
+let age4= 89;
+
+console.log(age1);
+console.log(age2);
+console.log(age3);
+console.log(age4);
+console.log
+
+let Greeting = 7;
+
+console.log(Greeting);
+
+
+let age = 7;
+let title = "Sveiki";
+let isGreen = true;
+let myWallet = true;
+let girlfiend = null;
